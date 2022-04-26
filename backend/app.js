dc8e862b7f52b8a1a051b1863ab7b758a9254777
SusuLaD <mathieucaillou@gmail.com>
2022-04-26 14:39:00 +0200

    petit change

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/user');
+const saucesRoutes = require("./routes/sauces");
+
 mongoose.connect('mongodb+srv://Mathieu:Martin123@cluster0.a92r9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -16,6 +19,7 @@ app.use((req, res, next) => {
     next();
   });
 
-  
+app.use("/api/auth", userRoutes);
+app.use('/api/sauces', saucesRoutes);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,21 +1,21 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const User = reqiure("../models/user");
+const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User ({
-            email: req.body.email,
-            password: hash
+      .then(hash => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
         });
         user.save()
-        .then(() => res.status(201).json({ message: "Utilisateur crée !"}))
-        .catch(()=> res.status(400).json({ error}))
-    })
-    .catch(error => res.status(500).json({ error}));
-};
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .catch(error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
@@ -29,7 +29,7 @@ exports.login = (req, res, next) => {
                 return res.status(401).json({ error: "Mot de passe incorrect !"});
             }
             res.status(200).json({
-                userId: super._id,
+                userId: user._id,
                 token: jwt.sign(
                     { userId: user._id },
                     "RANDOM_TOKEN_SECRET",
@@ -37,7 +37,7 @@ exports.login = (req, res, next) => {
                 )
             });
         })
-        .catch(error => res.status(500).json({ error}));
+        .catch(error => res.status(500).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
 };
